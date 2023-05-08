c50b0710d24ec7559dfd24109c6936d8d9eb97a4
joisilham9 <127075874+joisilham9@users.noreply.github.com>
2023-05-08 10:46:42 +0700

    Add project files.

---
+++ pointer1/pointer1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+class mahasiswa {
+public:
+	int nim;
+	void showNim() {
+		cout<< "No induk = " <<nim<<endl;
+	}
+};
+
+int main() {
+	mahasiswa mhs
+}
